131debcbce907b91811d3f83be44f1989f62393b
Joanna Kao <joanna.kao@ft.com>
2017-09-29 16:37:25 -0400

    get story content from google doc, not spreadsheet cell

--- config/index.js
+++ config/index.js
@@ -1,7 +1,11 @@
-import * as bertha from 'bertha-client';
+import structuredGoogleDoc from 'structured-google-docs-client';
 import article from './article';
 import getFlags from './flags';
 import getOnwardJourney from './onward-journey';
+
+async function transform(a, b) {
+  return `<span class="g-audio">${a}<audio controls><source src="${b}" type="audio/mpeg"></audio></span>`;
+}
 
 export default async (a, storyId, storyMetadata) => {
   const d = await article();
@@ -9,7 +13,7 @@ export default async (a, storyId, storyMetadata) => {
   const onwardJourney = await getOnwardJourney();
 
   d.id = storyMetadata.uuid;
-  d.url = `https://ig.ft.com/life-of-a-song/${storyMetadata.storyId}.html`;
+  d.url = `https://ig.ft.com/life-of-a-song/${storyId}.html`;
   d.headline = storyMetadata.headline;
   d.title = d.headline;
   d.summary = storyMetadata.standfirst;
@@ -19,9 +23,8 @@ export default async (a, storyId, storyMetadata) => {
   d.publishedDate = new Date(storyMetadata.pubdate);
   d.byline = [{ name: storyMetadata.author, url: storyMetadata.authorlink }];
 
-  const textContent = await bertha.get('1B-nm2Cip5AU57KC9Yt03WM0JB5jSxNL0CFjJmyN2upo', [storyId], { republish: true }).then(data => data[storyId]);
-
-  const storyContent = textContent[0].text.replace('ft-ig-audio-prod.s3.amazonaws.com', 'ig-audio.ft.com');
+  const textContent = await structuredGoogleDoc(storyMetadata.googledocid, { transform });
+  const storyContent = (textContent ? textContent.replace('ft-ig-audio-prod.s3.amazonaws.com', 'ig-audio.ft.com') : textContent);
 
   return {
     ...d,
