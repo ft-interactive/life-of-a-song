cf55992c1299c87d4ef00afabf9c12382d0f1146
Joanna S. Kao <joannaskao@gmail.com>
2017-10-04 12:23:02 -0400

    update to latest onward journey (#9)

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,5 @@
 export default () => ({
   relatedContent: [
-    { rows: 1, list: 'thing/NzJkMzQ4OGItZGU0MC00ZDNkLTk0NzgtYTU1NTM1NDQ5ZmNl-QnJhbmRz' },
-    { rows: 1, list: 'thing/MTcw-U2VjdGlvbnM=' },
+    { rows: 1, list: 'thing/1eb4d2f2-618a-39ea-99cf-5cf11287e853' },
   ],
 });
