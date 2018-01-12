927d3bdee0e34888d663188f345dccb84abaf4e8
Joanna Kao <joanna.kao@ft.com>
2018-01-12 14:37:53 -0500

    point to one-off audio stories content sheet

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -211,7 +211,7 @@ gulp.task('build-pages', async () => {
   delete require.cache[require.resolve('./config/article')];
   delete require.cache[require.resolve('./config/index')];
 
-  const toc = await bertha.get('1B-nm2Cip5AU57KC9Yt03WM0JB5jSxNL0CFjJmyN2upo', ['toc'], { republish: true }).then(data => data.toc);
+  const toc = await bertha.get('1lG-G-d1XPYLjt2eiPwKCvpwz-EUKkQUazQXAqfrOY_U', ['toc'], { republish: true }).then(data => data.toc);
 
   const storyIds = toc.map(d => d.id);
   for (let i = 0; i < storyIds.length; i += 1) {
