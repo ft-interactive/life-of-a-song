f5ba3c1a96ef16cb1a11995d3db1a5dc76b0a34e
Joanna S. Kao <joanna.kao@ft.com>
2022-03-17 23:20:40 +0000

    Update gulp, CircleCI config to 2.1, now works on node 16 (#37)
    * Attemping to update gulp
    
    * Successfully build page
    
    * Fix gulp to work on dev, use single quotes
    
    * Update to circleci 2.1
    
    * Remove bower_components bit of circleci workflow
    
    * Bring back qa task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,7 +6,6 @@ import browserSync from 'browser-sync';
 import gulp from 'gulp';
 import mergeStream from 'merge-stream';
 import path from 'path';
-import runSequence from 'run-sequence';
 import source from 'vinyl-source-stream';
 import watchify from 'watchify';
 import AnsiToHTML from 'ansi-to-html';
@@ -16,11 +15,13 @@ import htmlmin from 'gulp-htmlmin';
 import rev from 'gulp-rev';
 import revReplace from 'gulp-rev-replace';
 import gulpdata from 'gulp-data';
-import sass from 'gulp-sass';
 import util from 'gulp-util';
 import autoprefixer from 'gulp-autoprefixer';
-import plumber from 'gulp-plumber';
 import rename from 'gulp-rename';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+
+const sass = gulpSass(dartSass);
 
 const ansiToHTML = new AnsiToHTML();
 
@@ -32,18 +33,11 @@ const AUTOPREFIXER_BROWSERS = [
   'Safari >= 7',
 ];
 
-const BROWSERIFY_ENTRIES = [
-  'index.js',
-];
+const BROWSERIFY_ENTRIES = ['index.js'];
 
-const BROWSERIFY_TRANSFORMS = [
-  'babelify',
-  'debowerify',
-];
+const BROWSERIFY_TRANSFORMS = ['babelify', 'debowerify'];
 
-const OTHER_SCRIPTS = [
-  'components/core/top.js',
-];
+const OTHER_SCRIPTS = ['components/core/top.js'];
 
 const exec = require('child_process').exec;
 
@@ -55,23 +49,14 @@ const copyGlob = OTHER_SCRIPTS.concat([
 
   // REPLACE: if using imagmin
   // '!client/**/*.{jpg,png,gif,svg}',
-
 ]);
 
-const delay = ms => new Promise((resolve) => {
-  setTimeout(resolve, ms);
-});
+const delay = (ms) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
 
 // helpers
-let preventNextReload; // hack to keep a BS error notification on the screen
-function reload() {
-  if (preventNextReload) {
-    preventNextReload = false;
-    return;
-  }
-
-  browserSync.reload();
-}
 
 function handleBuildError(headline, error) {
   if (process.env.NODE_ENV === 'development') {
@@ -79,14 +64,12 @@ function handleBuildError(headline, error) {
     util.log(headline, error && error.stack);
 
     // report it in browser sync
-    let report = (
-      `<span style="color:red;font-weight:bold;font:bold 20px sans-serif">${headline}</span>`
-    );
+    let report = `<span style="color:red;font-weight:bold;font:bold 20px sans-serif">${headline}</span>`;
 
     if (error) {
-      report += (
-        `<pre style="text-align:left;max-width:800px">${ansiToHTML.toHtml(error.stack)}</pre>`
-      );
+      report += `<pre style="text-align:left;max-width:800px">${ansiToHTML.toHtml(
+        error.stack,
+      )}</pre>`;
     }
 
     browserSync.notify(report, 60 * 60 * 1000);
@@ -110,7 +93,8 @@ function getBundlers(useWatchify) {
       }),
 
       execute() {
-        const stream = this.b.bundle()
+        const stream = this.b
+          .bundle()
           .on('error', function browserifyError(error) {
             handleBuildError.call(this, 'Error building JavaScript', error);
           })
@@ -133,14 +117,16 @@ function getBundlers(useWatchify) {
     };
 
     // register all the transforms
-    BROWSERIFY_TRANSFORMS.forEach(transform => bundler.b.transform(transform));
+    BROWSERIFY_TRANSFORMS.forEach((transform) =>
+      bundler.b.transform(transform)
+    );
 
     // upgrade to watchify if we're in 'serve' mode
     if (useWatchify) {
       bundler.b = watchify(bundler.b);
       bundler.b.on('update', () => {
         // re-run the bundler then reload the browser
-        bundler.execute().on('end', reload);
+        bundler.execute().on('end', 'reload');
       });
     }
 
@@ -152,75 +138,42 @@ function getBundlers(useWatchify) {
  * The Tasks
  */
 
-// makes a production build (client => dist)
-gulp.task('default', (done) => {
-  process.env.NODE_ENV = 'production';
-  runSequence(
-    ['copy'],
-    ['scripts', 'styles', 'build-pages'],
-    ['html'/* 'images' */],
-    ['revreplace'],
-  done);
+// reload
+let preventNextReload; // hack to keep a BS error notification on the screen
+gulp.task('reload', (cb) => {
+  if (preventNextReload) {
+    preventNextReload = false;
+    return;
+  }
+  browserSync.reload();
+  cb();
 });
 
-// runs a development server (serving up dist and client)
-gulp.task('watch', ['styles', 'build-pages', 'copy'], (done) => {
-  const bundlers = getBundlers(true);
-
-  // execute all the bundlers once, up front
-  const initialBundles = mergeStream(bundlers.map(bundler => bundler.execute()));
-  initialBundles.resume(); // (otherwise never emits 'end')
-
-  initialBundles.on('end', () => {
-    // use browsersync to serve up the development app
-    browserSync({
-      notify: true,
-      open: process.argv.includes('--open'),
-      ui: process.argv.includes('--bsui'),
-      ghostMode: process.argv.includes('--ghost'),
-      port: process.env.PORT || '3000',
-      server: {
-        baseDir: 'dist',
-      },
-    });
-
-    // refresh browser after other changes
-    gulp.watch([
-      'client/**/*.{html,md}',
-      'views/**/*.{js,html}',
-      'config/*.{js,json}'], ['build-pages', reload]);
-    gulp.watch(['client/**/*.scss'], ['styles', reload]);
-    gulp.watch(copyGlob, ['copy', reload]);
-
-    // UNCOMMENT IF USING IMAGEMIN
-    // gulp.watch(['client/images/**/*'], reload);
-
-    done();
-  });
-});
-
-// copies over miscellaneous files (client => dist)
-gulp.task('copy', () =>
-  gulp.src(copyGlob, { dot: true })
-    .pipe(gulp.dest('dist')),
-);
-
-gulp.task('build-pages', async () => {
+gulp.task('build-pages', async (cb) => {
   delete require.cache[require.resolve('./views')];
   delete require.cache[require.resolve('./config/flags')];
   delete require.cache[require.resolve('./config/article')];
   delete require.cache[require.resolve('./config/index')];
 
-  const toc = await bertha.get('1B-nm2Cip5AU57KC9Yt03WM0JB5jSxNL0CFjJmyN2upo', ['toc'], { republish: true }).then(data => data.toc);
+  const toc = await bertha
+    .get('1B-nm2Cip5AU57KC9Yt03WM0JB5jSxNL0CFjJmyN2upo', ['toc'], {
+      republish: true,
+    })
+    .then((data) => data.toc);
 
-  const storyIds = toc.map(d => d.id);
+  const storyIds = toc.map((d) => d.id);
   for (let i = 0; i < storyIds.length; i += 1) {
     const storyId = storyIds[i];
-    const storyMetadata = toc.filter(s => s.id === storyId)[0];
+    const storyMetadata = toc.filter((s) => s.id === storyId)[0];
 
-    gulp.src('client/**/*.html')
-      .pipe(plumber())
-      .pipe(gulpdata(async d => require('./config').default(d, storyId, storyMetadata)))
+    gulp
+      .src('client/**/*.html')
+      // .pipe(plumber())
+      .pipe(
+        gulpdata(async (d) =>
+          require('./config').default(d, storyId, storyMetadata)
+        )
+      )
       .pipe(gulpnunjucks.compile(null, { env: require('./views').configure() }))
       .pipe(rename(`${storyId}.html`))
       .pipe(gulp.dest('dist'));
@@ -230,52 +183,117 @@ gulp.task('build-pages', async () => {
       await delay(4000);
     }
   }
+
+  cb();
+});
+
+// copies over miscellaneous files (client => dist)
+gulp.task('copy', (cb) => {
+  gulp.src(copyGlob, { dot: true, allowEmpty: true }).pipe(gulp.dest('dist'));
+  cb();
 });
 
 // minifies all HTML, CSS and JS (dist & client => dist)
-gulp.task('html', () =>
-  gulp.src('dist/**/*.html')
+gulp.task('html', (cb) => {
+  gulp
+    .src('dist/**/*.html')
     .pipe(inlineSource())
-    .pipe(htmlmin({
-      collapseWhitespace: true,
-      processConditionalComments: true,
-    }))
-    .pipe(gulp.dest('dist')),
-);
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        processConditionalComments: true,
+        minifyJS: true,
+      })
+    )
+    .pipe(gulp.dest('dist'));
+  cb();
+});
 
 // task to do a straightforward browserify bundle (build only)
-gulp.task('scripts', () =>
-  mergeStream(getBundlers().map(bundler => bundler.execute())),
-);
+gulp.task('scripts', (cb) => {
+  mergeStream(getBundlers().map((bundler) => bundler.execute()));
+  cb();
+});
 
 // builds stylesheets with sass/autoprefixer
-gulp.task('styles', () =>
-  gulp.src('client/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass({
-      includePaths: 'bower_components',
-      outputStyle: process.env.NODE_ENV === 'production' ? 'compressed' : 'expanded',
-    }).on('error', function sassError(error) {
-      handleBuildError.call(this, 'Error building Sass', error);
-    }))
-    .pipe(autoprefixer({ browsers: AUTOPREFIXER_BROWSERS }))
-    .pipe(gulp.dest('dist')),
-);
+gulp.task('styles', (cb) => {
+  gulp
+    .src('client/**/*.scss')
+    .pipe(
+      sass({
+        includePaths: 'node_modules',
+        outputStyle:
+          process.env.NODE_ENV === 'production' ? 'compressed' : 'expanded',
+      }).on('error', function sassError(error) {
+        handleBuildError.call(this, 'Error building Sass', error);
+      })
+    )
+    .pipe(autoprefixer({ overrideBrowserslist: AUTOPREFIXER_BROWSERS }))
+    .pipe(gulp.dest('dist'));
+  cb();
+});
 
 // renames asset files and adds a rev-manifest.json
-gulp.task('revision', () =>
-  gulp.src(['dist/**/*.css', 'dist/**/*.js'])
+gulp.task('revision', (cb) => {
+  gulp
+    .src(['dist/**/*.css', 'dist/**/*.js'])
     .pipe(rev())
     .pipe(gulp.dest('dist'))
     .pipe(rev.manifest())
-    .pipe(gulp.dest('dist')),
-);
+    .pipe(gulp.dest('dist'));
+  cb();
+});
 
 // edits html to reflect changes in rev-manifest.json
-gulp.task('revreplace', ['revision'], () =>
-  gulp.src('dist/**/*.html')
+gulp.task('revreplace', gulp.series('revision'), (cb) => {
+  gulp
+    .src('dist/**/*.html')
     .pipe(revReplace({ manifest: gulp.src('./dist/rev-manifest.json') }))
-    .pipe(gulp.dest('dist')),
+    .pipe(gulp.dest('dist'));
+  cb();
+});
+
+// runs a development server (serving up dist and client)
+gulp.task(
+  'watch',
+  gulp.series([
+    'styles',
+    'build-pages',
+    'copy',
+    (done) => {
+      const bundlers = getBundlers(true);
+
+      // execute all the bundlers once, up front
+      const initialBundles = mergeStream(
+        bundlers.map((bundler) => bundler.execute())
+      );
+      initialBundles.resume(); // (otherwise never emits 'end')
+
+      initialBundles.on('end', () => {
+        // use browsersync to serve up the development app
+        browserSync({
+          notify: true,
+          open: process.argv.includes('--open'),
+          ui: process.argv.includes('--bsui'),
+          ghostMode: process.argv.includes('--ghost'),
+          port: process.env.PORT || '3000',
+          server: {
+            baseDir: 'dist',
+          },
+        });
+
+        // refresh browser after other changes
+        gulp.watch(['client/**/*.{html,md}', 'views/**/*.{js,html}', 'config/**/*'], gulp.series(['reload', 'build-pages']));
+        gulp.watch(['client/styles/**/*.scss'], gulp.series(['reload', 'styles']));
+        gulp.watch(copyGlob, gulp.series(['reload', 'copy']));
+
+        // UNCOMMENT IF USING IMAGEMIN
+        // gulp.watch(['client/images/**/*'], reload);
+
+        done();
+      });
+    },
+  ])
 );
 
 // IMAGE COMPRESSION:
@@ -302,3 +320,23 @@ gulp.task('qa', async () => {
     console.log(stderr);
   });
 });
+
+// makes a production build (client => dist)
+gulp.task(
+  'default',
+  gulp.series([
+    (done) => {
+      process.env.NODE_ENV = 'production';
+      done();
+    },
+    'copy',
+    'build-pages',
+    'styles',
+    'scripts',
+    'html',
+    'revreplace',
+    (cb) => {
+      cb();
+    },
+  ])
+);
